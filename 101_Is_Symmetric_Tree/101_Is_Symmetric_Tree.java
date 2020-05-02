66ae218e328c8d49c0f236d34ce7cf1bd5c940d7
Divya Porwal <porwaldivya8@gmail.com>
2020-05-02 16:16:40 -0500

    101 is_Symmetric_Tree

---
+++ 101_Is_Symmetric_Tree/101_Is_Symmetric_Tree.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) {
+            return true;
+        }
+        
+        return issymmetric(root.left, root.right);
+        
+    }
+    
+    public boolean issymmetric(TreeNode left,TreeNode right) {
+        if(left == null && right == null) {
+            return true;
+        } else if(left == null || right == null) {
+            return false;
+        }
+        
+        if(left.val != right.val) {
+            return false;
+        }
+        
+        return issymmetric(left.left,right.right) && issymmetric(left.right,right.left);
+        
+        
+    }
+}
